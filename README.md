3d034eb9ca9511a7d140f4abdf7ea294674613c9
Jenny Wilson <wils0986@algonquinlive.com>
2024-05-27 15:41:34 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# portfolio
-Jenny's Design Portfolio 2024
+# Jenny's Design Portfolio 2024
+
+I created this portfolio using **HTML**, **CSS** and a little bit of **JS**. I started by creating the basic **HTML** structure for all pages, then proceeded to create all of the content before proceeding to **CSS**. I find the work flow more clean and consistent when I start with the "foundation". From there, I make sure my **HTML** is semantically correct, and I proceed to styling with **CSS**.
+
+I used **CSS** to give the website the colours, layout, and responsive design that you see. The website is optimized for mobile, tablet and desktop screens with fully responsive fonts, images, links, icons, and more.
---
+++ css/main.js
@@ -0,0 +1,26 @@
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+
+  
+  /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
