4770b6ff9abcf03458028bc36fcd9dc1b0343de0
Jenny Wilson <wils0986@algonquinlive.com>
2024-06-02 16:23:25 -0400

    fixes contacts resonsiveness

--- main.js
+++ main.js
@@ -13,14 +13,3 @@ $(function() {
     });
   });
 
-  
-  /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
