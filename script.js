8a494c3065ad477accba7ea893da4f3b6e6b7a01
Jenny Wilson <wils0986@algonquinlive.com>
2025-12-18 12:48:15 -0500

    adjusts skillbars, project imgs, contact form

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ $(document).ready(function () {
       skillbarsAnimated = true;
       catAnimated = true;
     }
-  }
+  }  
 
   function resetAboutAnimations() {
     skillbarsAnimated = false;
